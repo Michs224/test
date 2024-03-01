82056cb4587fce7b99050247c990fcf15ebb9d89
Michael Geraldin Wijaya <michaelwjy2@gmail.com>
2024-03-01 21:07:19 +0700

    mcmc

--- hungry.py
+++ hungry.py
@@ -6,9 +6,3 @@ if hungry=="yes" and hungry=="Yyyy":
     print("Eat fries")
 else:
     thirsty=input("Are you thirsty? ")
-<<<<<<< HEAD
-    print("Drink water")
-=======
-    if thirsty=="yes":
-        pass
->>>>>>> ea3d6b674fe1662ffe658cc7f89fd217dfd2058a
