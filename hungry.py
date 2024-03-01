dd0200aea4330fb33136e686862aabebec544699
Michael Geraldin Wijaya <128117104+Michs224@users.noreply.github.com>
2024-03-01 21:22:11 +0700

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,10 +1,8 @@
 hungry=input("Are you hungry? ")
-if hungry=="yes" and hungry=="Yyyy":
+if hungry=="yes" and hungry=="Yo":
     print("Eat samora")
     print("Eat pizza")
     print("Eat burger")
     print("Eat fries")
 else:
     thirsty=input("Are you thirsty? ")
-    if thirsty=="yes":
-        print("Drink water")
